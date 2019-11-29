b8d6d38cb595512e93a4299d0508373d04481432
sivaramp1991 <psvram1991@gmail.com>
2019-11-29 21:17:01 +0530

    3 files modified

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ import './style.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {taskList:''};
+    this.state = {
+      taskList:'',
+       overdueTask: '',
+       assignedTask: '',
+       inQueueTask:  '' };
   }
   componentDidMount(){
-    
-    console.log(TaskData)
+   const overdueTaskList = TaskData.data.filter(i => { return i.status === "Overdue"});
+   const assignedTaskList = TaskData.data.filter(i => { return i.status === "Assigned"});
+   const inQueueTaskList = TaskData.data.filter(i => { return i.status === "In Queue"}) 
+   console.log(overdueTaskList) 
     this.setState(
-      {taskList:TaskData.data}
+      {taskList:TaskData.data,
+       overdueTask:   overdueTaskList,
+       assignedTask:   assignedTaskList,
+       inQueueTask:   inQueueTaskList}
     )
   }
 
@@ -35,7 +44,19 @@ class App extends Component {
       </div>
 
       <div className='taskStatus'>
-      <span className='overdueTask task-icon'></span>
+
+      <span className='overdueTask task-icon'>
+      <label className='overdueTaskLabel'>{this.state.overdueTask.length + "  Overdue"}</label>
+      </span>
+
+      <span className='assignedTask task-icon'>
+      <label className='assignedTaskLabel'>{this.state.assignedTask.length + " Assigned"}</label>
+      </span>
+
+      <span className='inQueueTask task-icon'>
+      <label className='inQueueTaskLabel'>{this.state.inQueueTask.length + " In Queue"}</label>
+      </span>
+
       <span className='timeEstimated task-icon'>
       <label className='label1'>Estimated Time</label>
       <label className='label2'>3 day(s)</label></span>
