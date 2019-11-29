eea5b2ebe4bda56345125aae71a02d2485124a93
sivaramp1991 <psvram1991@gmail.com>
2019-11-29 18:13:42 +0530

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class App extends Component {
     this.state = {taskList:''};
   }
   componentDidMount(){
-    debugger;
+    
     console.log(TaskData)
     this.setState(
       {taskList:TaskData.data}
@@ -21,13 +21,23 @@ class App extends Component {
     console.log(TaskData)
     return (
       <div className='overviewContainer'>
+      
       <div className='seprateContainer'>
-      <span className='taskbreak1'>{this.state.taskList.length}</span>
-      <span className='taskbreak2'/>
-      <span className='taskbreak3'/>
-      <span className='taskbreak4'/>
-      <span className='taskbreak5'/>
+      <span className='taskbreak1 seperate'/>
+      <span className='taskbreak2 seperate'/>
+      <span className='taskbreak3 seperate'/>
+      <span className='taskbreak4 seperate'/>
+      <span className='taskbreak5 seperate'/>
+      <span >
+      <label className='allTaskCount'>{this.state.taskList.length}</label>
+      <label className='allTask'>Tasks</label>
+      </span>
       </div>
+
+      <div className='taskStatus'>
+      <span className='overdueTask'></span>
+      </div>
+      
         
       </div>
     );
